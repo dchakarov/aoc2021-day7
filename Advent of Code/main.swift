cc49a72f6b80bf4923c30b51c1709fc715e59323
Dimitar Chakarov <dimitar@dchakarov.com>
2021-12-07 23:03:46 +0000

    Challenge 1 + 2 solutions for Challenge 2 (brute force & binary search)

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -12,28 +12,90 @@ func main() {
     
     let lines = inputString.components(separatedBy: "\n")
         .filter { !$0.isEmpty }
-    
-    // Sample algorithm
-    var scoreboard = [String: Int]()
-    lines.forEach { line in
-        let (name, score) = parseLine(line)
-        scoreboard[name] = score
+
+    let positions = lines[0].components(separatedBy: ",").map { Int($0)! }
+
+    let min = positions.min()!
+    let max = positions.max()!
+
+    var fuels = [Int]()
+    for i in min ... max {
+        fuels.append(calcFuel(crabs: positions, anchor: i))
     }
-    scoreboard
-        .sorted { lhs, rhs in
-            lhs.value > rhs.value
-        }
-        .forEach { name, score in
-            print("\(name) \(score) pts")
-        }
+
+    print(fuels.min()!)
 }
 
-func parseLine(_ line: String) -> (name: String, score: Int) {
-    let helper = RegexHelper(pattern: #"([\-\w]*)\s(\d+)"#)
-    let result = helper.parse(line)
-    let name = result[0]
-    let score = Int(result[1])!
-    return (name: name, score: score)
+func calcFuel(crabs: [Int], anchor: Int) -> Int {
+    crabs.reduce(0) { partialResult, crab in
+        partialResult + abs(crab - anchor)
+    }
 }
 
-main()
+func main2() {
+    let fileUrl = URL(fileURLWithPath: "./aoc-input")
+    guard let inputString = try? String(contentsOf: fileUrl, encoding: .utf8) else { fatalError("Invalid input") }
+
+    let lines = inputString.components(separatedBy: "\n")
+        .filter { !$0.isEmpty }
+
+    let positions = lines[0].components(separatedBy: ",").map { Int($0)! }
+
+    let min = positions.min()!
+    let max = positions.max()!
+
+    var fuels = [Int]()
+    for i in min ... max {
+        fuels.append(calcFuel2(crabs: positions, anchor: i))
+    }
+
+    print(fuels.min()!)
+}
+
+func main3() {
+    let fileUrl = URL(fileURLWithPath: "./aoc-input")
+    guard let inputString = try? String(contentsOf: fileUrl, encoding: .utf8) else { fatalError("Invalid input") }
+
+    let lines = inputString.components(separatedBy: "\n")
+        .filter { !$0.isEmpty }
+
+    let positions = lines[0].components(separatedBy: ",").map { Int($0)! }
+
+    let min = positions.min()!
+    let max = positions.max()!
+
+    let result = search(start: min, end: max, positions: positions)
+
+    print(result)
+}
+
+func search(start: Int, end: Int, positions: [Int]) -> Int {
+    let mid = (start + end) / 2
+    let fuelMin = calcFuel2(crabs: positions, anchor: start)
+    let fuelMax = calcFuel2(crabs: positions, anchor: end)
+
+    if mid == end || mid == start {
+        return min(fuelMin, fuelMax)
+    }
+
+    if fuelMin < fuelMax {
+        return search(start: start, end: mid, positions: positions)
+    } else {
+        return search(start: mid, end: end, positions: positions)
+    }
+}
+
+func calcFuel2(crabs: [Int], anchor: Int) -> Int {
+    crabs.reduce(0) { partialResult, crab in
+        let distance = abs(crab - anchor)
+        var fuel = 0
+        for i in 0 ... distance {
+            fuel += i
+        }
+        return partialResult + fuel
+    }
+}
+
+//main()
+//main2()
+main3() // binary search to solve #2
